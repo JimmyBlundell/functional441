e68f0c55211ea1c9167a100f82b2ceb12e4bb51e
Jimmy Blundell <jblundell123@gmail.com>
2020-05-07 14:26:03 -0500

    Found first order entropy for single char split.

--- functional_stuff.py
+++ functional_stuff.py
@@ -2,24 +2,32 @@ import functools
 import math
 from collections import Counter
 
-equation = lambda nc, pc : (nc)*(-1)*(pc)*(math.log2(pc))
+# Lambda expression for finding total info
+equation = lambda x: (x)*(-1)*(x/num_chars)*(math.log2(x/num_chars))
 
 text = open("WarAndPeace.txt")
 
 text = str(list(text))
 
+num_chars = len(text)
+
 char_freq = {}
 
-def getcharfreq(charfreq, ch):
-    charfreq[ch] = charfreq.get(ch, 0) + 1
-    return charfreq
+def getcharfreq(char_freq, ch):
+    char_freq[ch] = char_freq.get(ch, 0) + 1
+    return char_freq
 
 # Get the frequencies of each character in the text
 functools.reduce(getcharfreq, text, char_freq)
 
-print(char_freq)
+#total_chars = len(char_freq) # Use this in lambda equation to get probability of characters: #occurences/#characters
 
-total_chars = len(char_freq) # Use this in lambda equation to get probability of characters: #occurences/#characters
+items = list(char_freq.items()) # Turn into list to iterate through in map() function - Don't know if I need?
 
-#TODO: Find a way to use the equation to do a summation over all the char frequencies in char_freq to get total information
-#TODO: Repeat for pairs and triples, then figure out concurrency.
+test = list(map(equation, char_freq.values()))
+
+single_char_info = sum(test)
+
+print(single_char_info
+      )
+# TODO: Repeat for pairs and triples, then figure out concurrency.
