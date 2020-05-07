9f1491143d294744401e837332c87c5d69b176d2
Jimmy Blundell <jblundell123@gmail.com>
2020-05-07 14:45:15 -0500

    Wrapped in function - lots of cleaning up to do still.

--- functional_stuff.py
+++ functional_stuff.py
@@ -2,31 +2,34 @@ import functools
 import math
 from collections import Counter
 
-# Lambda expression for finding total info
-equation = lambda x: (x)*(-1)*(x/num_chars)*(math.log2(x/num_chars))
-
 text = open("WarAndPeace.txt")
 
 text = str(list(text))
 
-num_chars = len(text) # Used for calculating probability
+num_single_chars = len(text) # Used for calculating probability
 
-char_freq = {}
+# Lambda expression for finding total info
+equation = lambda x: (x) * (-1) * (x / num_single_chars) * (math.log2(x / num_single_chars))
 
-def getcharfreq(char_freq, ch):
-    char_freq[ch] = char_freq.get(ch, 0) + 1
-    return char_freq
+def get_single_char_info(equation, text):
 
-# Get the frequencies of each character in the text
-functools.reduce(getcharfreq, text, char_freq)
+    char_freq = {}
 
-# Using map() to apply entropy equation on each frequency in char_freq, outputting list
-test = list(map(equation, char_freq.values()))
+    def getcharfreq(char_freq, ch):
+        char_freq[ch] = char_freq.get(ch, 0) + 1
+        return char_freq
 
-# Sum together values in list for total info
-single_char_info = sum(test)
+    # Get the frequencies of each character in the text
+    functools.reduce(getcharfreq, text, char_freq)
 
-print(single_char_info)
+    # Using map() to apply entropy equation on each frequency in char_freq, outputting list
+    test = list(map(equation, char_freq.values()))
 
+    # Sum together values in list for total info
+    single_char_info = sum(test)
+
+    print(single_char_info)
+
+get_single_char_info(equation, text)
 
 # TODO: Repeat for pairs and triples, then figure out concurrency.
