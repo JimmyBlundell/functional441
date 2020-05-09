f9fd8384583bd671b93ade40749df3f276a8fe7a
Jimmy Blundell <jblundell123@gmail.com>
2020-05-08 19:00:22 -0500

    It's all messed up :)

--- functional_stuff.py
+++ functional_stuff.py
@@ -6,7 +6,62 @@ import timeit
 #from double_split import get_double_char_info
 #from triple_split import get_triple_char_info
 
-def get_single_char_info(text):
+
+
+def get_char_info():
+
+    ans = []
+
+    text = open("WarAndPeace.txt")
+
+    # Operate on 3 different strings? Not sure if needed yet
+    text_single = str(list(text))
+    text_double = str(list(text))
+    text_triple = str(list(text))
+
+    num_single_chars = len(text_single)
+    num_double_chars = num_single_chars/2
+    num_triple_chars = num_single_chars/3
+
+
+    # Lambda equations for calculating entropy for each split
+    single_split_equation = lambda x: (x) * (-1) * (x / num_single_chars) * (math.log2(x / num_single_chars))
+    double_split_equation = lambda x: (x) * (-1) * (x / num_double_chars) * (math.log2(x / num_double_chars))
+    triple_split_equation = lambda x: (x) * (-1) * (x / num_triple_chars) * (math.log2(x / num_triple_chars))
+
+    # Dictionaries to hold the frequencies of chars depending on split
+    single_char_freq = {}
+    double_char_freq = {}
+    triple_char_freq = {}
+
+    def getcharfreq(char_freq, ch):
+        char_freq[ch] = char_freq.get(ch, 0) + 1
+        return char_freq
+
+    # Get the character frequencies, finally
+    functools.reduce(getcharfreq, text, single_char_freq)
+    functools.reduce(getcharfreq, text, double_char_freq)
+    functools.reduce(getcharfreq, text, triple_char_freq)
+
+    #TODO: map() function is not working right now - need to find out why
+    # Using map() to apply entropy equation on each frequency in char_freq, outputting list
+    lst_single = list(map(single_split_equation, single_char_freq.values()))
+    lst_double = list(map(double_split_equation, double_char_freq.values()))
+    lst_triple = list(map(triple_split_equation, triple_char_freq.values()))
+
+    # Sum together values in list for total info
+    single_char_info = sum(lst_single)
+    double_char_info = sum(lst_double)
+    triple_char_info = sum(lst_triple)
+
+    # Return a list of final values
+    print("Single character split: " + str(single_char_info))
+    print("Double character split: " + str(double_char_info))
+    print("Triple character split: " + str(triple_char_info))
+
+    #return ans
+
+"""def get_single_char_info(text):
 
     num_single_chars = len(text)
 
@@ -83,20 +138,13 @@ def get_triple_char_info(text):
 
     ans = str("Triple character split: " + str(triple_char_info))
     return ans
-
-
+"""
 
 def parellelize_me():
     start = timeit.default_timer()
 
-    text = open("WarAndPeace.txt")
-
-    text_single = str(list(text))
-    text_double = str(list(text))
-    text_triple = str(list(text))
 
     p = Pool(16)
-    ans = p.map(get_single_char_info, text)
 
 
     # TODO: Concurrency
@@ -109,7 +157,7 @@ def parellelize_me():
 
 
 if __name__ == '__main__':
-    parellelize_me()
+    get_char_info()
 
 
 
